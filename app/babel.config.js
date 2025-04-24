ce3f583a7803137a36fc403196a7081be5be2571
fluteur <malo.leroy@student-cs.fr>
2025-04-24 11:35:39 +0200

    Update Babel configuration for Jest compatibility and add metro-react-native-babel-preset dependency
    Fix CI errors from the last commit

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+/* eslint-disable no-undef */
+// For Jest compatibility
+const config = {
     presets: [
         ['babel-preset-expo'],
         ["module:metro-react-native-babel-preset"],
@@ -8,3 +10,10 @@ module.exports = {
         // Vous pouvez ajouter d'autres plugins ici si nécessaire
     ],
 };
+
+// Export in a way that works with both ESLint and Jest
+if (typeof module !== 'undefined') {
+    module.exports = config;
+}
+
+export default config;
