b5eb1dafdaaca72420bf2756d22caecb7f6d3ab4
Malo Leroy <malo.leroy@student-cs.fr>
2025-04-29 13:53:19 +0200

    Update build files

--- appKotlin/AppV1/app/build.gradle.kts
+++ appKotlin/AppV1/app/build.gradle.kts
@@ -40,17 +40,17 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation("androidx.compose.material:material-icons-extended") // <= ajoute ceci
+    implementation(libs.androidx.material.icons.extended) // <= ajoute ceci
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
