d70a6a8640e8932e2109b87560eca48a7458daf9
Nicolas <nicolas.barriere27@gmail.com>
2025-05-14 10:31:31 +0200

    Devider

--- appKotlin/AppV1/app/src/main/java/com/example/appv1/ui/screens/report/NewReportScreen.kt
+++ appKotlin/AppV1/app/src/main/java/com/example/appv1/ui/screens/report/NewReportScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Divider
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
