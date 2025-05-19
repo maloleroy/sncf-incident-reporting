f2ceeccdb8c31786508a61a1f23f9902702905cd
Malo Leroy <malo.leroy@student-cs.fr>
2025-05-19 14:50:09 +0200

    Optimize imports in NewReportScreen

--- appKotlin/AppV1/app/src/main/java/com/example/appv1/ui/screens/report/NewReportScreen.kt
+++ appKotlin/AppV1/app/src/main/java/com/example/appv1/ui/screens/report/NewReportScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -51,6 +50,7 @@ import androidx.core.content.ContextCompat
 import com.example.appv1.GemmaEngine
 import com.example.appv1.api.ChatMessage
 import com.example.appv1.api.ChatRequest
+import com.example.appv1.api.IncidentObjectsListRequest
 import com.example.appv1.api.RetrofitInstance
 import com.example.appv1.ui.components.NewReportScreenDivider
 import kotlinx.coroutines.CoroutineScope
