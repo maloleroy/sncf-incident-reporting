b656387742ad83dde842eea17b7704e03846ef64
Malo Leroy <malo.leroy@student-cs.fr>
2025-05-23 15:50:32 +0200

    Hotfix for imports in ConfirmationScreen.kt

--- appKotlin/AppV1/app/src/main/java/com/example/appv1/ui/screens/report/ConfirmationScreen.kt
+++ appKotlin/AppV1/app/src/main/java/com/example/appv1/ui/screens/report/ConfirmationScreen.kt
@@ -7,9 +7,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
