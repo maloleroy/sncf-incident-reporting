b48f745ff9d5d24cdd1b77213834e523a5aa7f7c
Malo Leroy <malo.leroy@student-cs.fr>
2025-05-23 15:45:45 +0200

    Fix tests

--- back/test_main.py
+++ back/test_main.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 
 from main import app
 from security import get_security_header
+from model import IncidentAnalysisResponse
 
 from env import load_dotenv
 load_dotenv()
@@ -21,22 +22,7 @@ def test_get_health():
     assert response.json() == {"status": "ok"}
 
 def test_create_incident():
-    incident_data = {
-        "id": 0,
-        "lastUpdate": 1747384018400,
-        "location": {
-            "id": 0,
-            "main": "ASCT_LOCAL",
-            "precision1": "NONE",
-            "precision2": "NONE",
-            "precision3": "NONE"
-        },
-        "subSystem": "NONE",
-        "failure": "NONE",
-        "comment": "This is a test comment",
-        "sealed": False,
-        "t4Call": False
-    }
+    incident_data = IncidentAnalysisResponse.model_config["json_schema_extra"]["examples"][0]
     response = client.post("/incidents/", json=incident_data, headers=get_security_header())
     assert response.status_code == 200
     assert response.json() == {"message": "Incident created successfully"}
@@ -45,16 +31,9 @@ def test_create_incident():
     assert response.status_code == 200
     incidents = response.json()
     assert len(incidents) > 0
-    assert incidents[-1]["lastUpdate"] == incident_data["lastUpdate"]
-    assert incidents[-1]["location"]["main"] == incident_data["location"]["main"]
-    assert incidents[-1]["location"]["precision1"] == incident_data["location"]["precision1"]
-    assert incidents[-1]["location"]["precision2"] == incident_data["location"]["precision2"]
-    assert incidents[-1]["location"]["precision3"] == incident_data["location"]["precision3"]
-    assert incidents[-1]["subSystem"] == incident_data["subSystem"]
-    assert incidents[-1]["failure"] == incident_data["failure"]
-    assert incidents[-1]["comment"] == incident_data["comment"]
-    assert incidents[-1]["sealed"] == incident_data["sealed"]
-    assert incidents[-1]["t4Call"] == incident_data["t4Call"]
+    for i in incident_data:
+        assert i in incidents[-1]
+        assert incident_data[i] == incidents[-1][i]
 
 def test_read_incidents():
     response = client.get("/incidents/", headers=get_security_header())
@@ -64,28 +43,10 @@ def test_read_incidents():
     if incidents:
         for incident in incidents:
             assert isinstance(incident, dict)
-            assert "id" in incident
-            assert "lastUpdate" in incident
-            assert "location" in incident
-            assert "subSystem" in incident
-            assert "failure" in incident
-            assert "comment" in incident
-            assert "sealed" in incident
-            assert "t4Call" in incident
-            assert isinstance(incident["location"], dict)
-            assert "id" in incident["location"]
-            assert "main" in incident["location"]
-            assert "precision1" in incident["location"]
-            assert "precision2" in incident["location"]
-            assert "precision3" in incident["location"]
-            assert isinstance(incident["location"]["main"], str)
-            assert isinstance(incident["location"]["precision1"], str)
-            assert isinstance(incident["location"]["precision2"], str)
-            assert isinstance(incident["location"]["precision3"], str)
-            assert isinstance(incident["subSystem"], str)
-            assert isinstance(incident["failure"], str)
-            assert isinstance(incident["comment"], str)
-            assert isinstance(incident["sealed"], bool)
-            assert isinstance(incident["t4Call"], bool)
+            for key in IncidentAnalysisResponse.model_fields.keys():
+                if key in incident:
+                    assert isinstance(incident[key], str)
+                else:
+                    assert key not in incident
     else:
         assert incidents == []
