9f62a66f7f10f8e7ed8502e4e37010f52c0ff930
Malo Leroy <malo.leroy@student-cs.fr>
2025-05-26 11:11:04 +0200

    Clean imports in MainActivity

--- appKotlin/AppV1/app/src/main/java/com/example/appv1/MainActivity.kt
+++ appKotlin/AppV1/app/src/main/java/com/example/appv1/MainActivity.kt
@@ -3,17 +3,15 @@ package com.example.appv1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.appv1.ui.screens.home.HomeScreen
+import com.example.appv1.ui.screens.report.ConfirmationScreen
 import com.example.appv1.ui.screens.report.NewReportScreen
-import com.example.appv1.ui.screens.report.ConfirmationScreen
-import com.example.appv1.ui.screens.report.ConfirmationScreen
-import com.example.appv1.ui.theme.AppV1Theme
 import com.example.appv1.ui.screens.report.ReportSharedViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.appv1.api.IncidentAnalysisResponse
+import com.example.appv1.ui.theme.AppV1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +43,7 @@ class MainActivity : ComponentActivity() {
                             response = sharedViewModel.lastIncidentAnalysisResponse,
                             onBack = { navController.popBackStack() },
                             trainType = sharedViewModel.trainType!!,
-                            car = sharedViewModel.trainCar!!
+                            trainCar = sharedViewModel.trainCar!!,
                         )
                     }
                 }
