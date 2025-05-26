d2177782d21d90c3806118f2291d56b3c02be267
Malo Leroy <malo.leroy@student-cs.fr>
2025-05-26 14:44:24 +0200

    Use Mistral large model instead of small

--- back/incident.py
+++ back/incident.py
@@ -10,7 +10,8 @@ logger = logging.getLogger(__name__)
 
 def find_incident(db, train, voiture, transcription) -> IncidentAnalysisResponse:
     possibilities = get_incidents(db, train, voiture)
-    response = "('Place', '', 'Siège', '', 'Accoudoir', '', '', 'Cassé, dégradé, manquant')" # get_response(transcription, possibilities)['content']
+    # response = "('Place', '', 'Siège', '', 'Accoudoir', '', '', 'Cassé, dégradé, manquant')"
+    response = get_response(transcription, possibilities)['content']
     logger.info("LLM Response: %s", response)
     if response.lower().replace(" ", "").replace(".", "") == "jenesaispas":
         raise HTTPException(status_code=404, detail="No incident found")
